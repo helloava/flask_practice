970e8fd8ea673bf090ebdf516e9dc48088128e32
ah young koo <helloavakoo@gmail.com>
2014-07-26 13:57:21 +0900

    crawling rocketpunch main title using flask_1st try

---
+++ views.py
@@ -0,0 +1,15 @@
+from flask import render_template, Flask, request
+from apps import app
+
+import urllib
+from bs4 import BeautifulSoup
+
+
+@app.route('/')
+def get():
+    return render_template("index.html")
+
+
+@app.route('/crawl', methods=['GET', 'POST'])
+def crawl():
+	return "http://rocketpun.ch/companies/" + request.form["page"] + "/"
